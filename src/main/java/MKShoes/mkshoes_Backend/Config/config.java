808a84c16227650673dc68b0107cbd0adb6e0067
nouasinguem <e.nouasinguemo.24@unimail.winchester.ac.uk>
2026-05-07 18:41:12 +0100

    Updated Origins in Cors filter

--- src/main/java/MKShoes/mkshoes_Backend/Config/config.java
+++ src/main/java/MKShoes/mkshoes_Backend/Config/config.java
@@ -36,7 +36,8 @@ public class config implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://mkshoes.netlify.app/", "http://localhost:5176",
+        "http://localhost:5173/", "http://localhost:5174/", "http://localhost:5175/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
