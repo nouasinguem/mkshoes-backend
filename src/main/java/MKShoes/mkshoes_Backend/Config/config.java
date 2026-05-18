c3f926194a4c222abbd186abfcdd98268a09dd11
nouasinguem <e.nouasinguemo.24@unimail.winchester.ac.uk>
2026-05-18 11:26:23 +0100

    updated the HTTPS cookies setting

--- src/main/java/MKShoes/mkshoes_Backend/Config/config.java
+++ src/main/java/MKShoes/mkshoes_Backend/Config/config.java
@@ -7,7 +7,6 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.List;
 @Configuration
 public class config implements WebMvcConfigurer {
@@ -26,6 +25,7 @@ public class config implements WebMvcConfigurer {
 
         return new CorsFilter(source);
     }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
@@ -33,11 +33,12 @@ public class config implements WebMvcConfigurer {
                         "classpath:/static/")
                 .setCachePeriod(0);
     }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://mkshoes.netlify.app", "http://localhost:5176",
-        "http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
+                        "http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
